f0457f87499cc9071915493656fb596e3daaa5ae
LJE <lje@juhyeongmaegpeulo.local>
2019-02-20 22:52:12 +0900

    Updates

---
+++ precache-manifest.0c6607779b7350cffbd5f20bc7cde073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "5237da5b03d0f045b8ea",
+    "url": "/movie_app/static/js/main.5237da5b.chunk.js"
+  },
+  {
+    "revision": "0a935515e47631cc43e9",
+    "url": "/movie_app/static/js/1.0a935515.chunk.js"
+  },
+  {
+    "revision": "5237da5b03d0f045b8ea",
+    "url": "/movie_app/static/css/main.0bf8db53.chunk.css"
+  },
+  {
+    "revision": "ec3c810e36bdb19025ce7c9d78403cae",
+    "url": "/movie_app/index.html"
+  }
+];
